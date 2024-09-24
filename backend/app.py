2c674c06ddbe8ce44095bedfc30777465e5db94f
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-09-24 18:33:43 +0200

    Notes for tomorrow.

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# Figure out how to fix this 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
